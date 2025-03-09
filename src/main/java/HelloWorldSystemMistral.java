4c66b5883199a0a7979f09d981c9a88716107c84
Frank Greco <fgreco@javasig.com>
2025-03-09 16:58:06 -0400

    Added popular GenAI service for european users

---
+++ src/main/java/HelloWorldSystemMistral.java
@@ -0,0 +1,35 @@
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.mistralai.MistralAiChatModelName;
+import dev.langchain4j.model.mistralai.MistralAiChatModel;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorldSystemMistral {
+    public static void main(String[] argv) {
+        ChatLanguageModel cmodel = MistralAiChatModel.builder()
+                .apiKey(System.getenv("MISTRAL_API_KEY"))
+                .modelName(String.valueOf(MistralAiChatModelName.OPEN_MISTRAL_7B))
+                .timeout(Duration.ofSeconds(120))
+                .maxTokens(256)
+                .build();
+
+        List<ChatMessage> messages = new ArrayList<>();
+
+        SystemMessage sysmsg = new SystemMessage("""
+                    You are a polite Java expert explaining concepts to a C++ software developer.
+                """);
+        messages.add(sysmsg);
+
+        UserMessage usrmsg = UserMessage.from("What are Java lambdas?");
+        messages.add(usrmsg);
+
+        var answer = cmodel.chat(messages);
+
+        System.out.println(answer.aiMessage().text());
+    }
+}
