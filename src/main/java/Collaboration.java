1a92167db0cc61963dc6f130b45d38660d660f1e
Frank Greco <fgreco@javasig.com>
2025-03-14 21:33:50 -0400

    Change separator line to show which LLM was working.

--- src/main/java/Collaboration.java
+++ src/main/java/Collaboration.java
@@ -21,26 +21,34 @@ public class Collaboration {
         userMessage = UserMessage.from("Give me a one-paragraph description of what machine learning is.");
         messages.add(userMessage);
         completion = sendToLLM("openai", messages);
+        System.out.println("[ORIGINAL]=======================\n" + completion);
 
         messages.add(UserMessage.from(completion));               // Add result to context
 
         /***** Send to Gemini *****/
         userMessage = UserMessage.from("An LLM described machine learning as "
-                + completion + " "
-                + "Rate this response from that other LLM in four different categories: "
-                + "correctness, conciseness, comprehensiveness and accuracy. "
-                + "Give it a grade from 0 to 10 for each category "
-                + "where 0 is not correct, not concise, incomprehensive and not accurate, "
-                + "and where 10 is 100 percent correct, extremely concise, perfectly understood, and 100 percent accurate.");
+                + completion + """
+                    Rate this response from that other LLM in four different categories: 
+                    correctness, conciseness, comprehensiveness and accuracy. 
+                    Give it a grade from 0 to 10 for each category 
+                    where 0 is not correct, not concise, incomprehensive and not accurate, 
+                    and where 10 is 100 percent correct, extremely concise, perfectly understood, and 100 percent accurate.
+                """);
+
         messages.add(userMessage);               // Add prompt to overall context
         completion = sendToLLM("gemini", messages);
+        System.out.println("[EVALUATION]=======================\n" + completion);
 
         messages.add(UserMessage.from(completion));
 
         /***** Send to Mistral *****/
-        userMessage = UserMessage.from("Rewrite the original LLM's response so that it rates 10 out of 10 for every category.");
+        userMessage = UserMessage.from("""
+            Modify the original LLM's response to
+            defining Machine Learning in one paragraph so that it rates
+            10 out of 10 for every category.""");
         messages.add(userMessage);
         completion = sendToLLM("mistral", messages);
+        System.out.println("[NEW]=======================\n" + completion);
     }
 
     /**
@@ -51,12 +59,12 @@ public class Collaboration {
      * @throws Exception
      */
     static String sendToLLM(String vendor, List<ChatMessage> chatMessages) throws Exception {
-        String lowerVendor = vendor.toLowerCase();
-        ChatLanguageModel clm = switch (lowerVendor) {
+        ChatLanguageModel clm = switch (vendor.toLowerCase()) {
             case "openai" ->
                 OpenAiChatModel.builder()
                         .apiKey(System.getenv("OPENAI_API_KEY"))
                         .modelName(OpenAiChatModelName.GPT_3_5_TURBO)
+                        .temperature(1.7)   // make it overly creative so there's opportunity to fix
                         .build();
             case "gemini" ->
                 GoogleAiGeminiChatModel.builder()
@@ -66,15 +74,14 @@ public class Collaboration {
             case "mistral" ->
                 MistralAiChatModel.builder()
                         .apiKey(System.getenv("MISTRAL_API_KEY"))
-                        .modelName(String.valueOf(MistralAiChatModelName.OPEN_MISTRAL_7B))
+                        //.modelName(String.valueOf(MistralAiChatModelName.OPEN_MISTRAL_7B))
+                        .modelName(MistralAiChatModelName.OPEN_MISTRAL_7B)
                         .build();
             default -> throw new Exception("Unknown GenAI model: " + vendor);
         };
         ChatResponse chatResponse = clm.chat(chatMessages);
         String completion = chatResponse.aiMessage().text();
-
-        System.err.println(completion);
-        System.out.println("=======================================");
+        
         return completion;
     }
 }
