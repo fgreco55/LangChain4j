60fb5338f2ab68f61a8e22014b31e627f6ad1416
Frank Greco <fgreco@javasig.com>
2025-04-17 17:34:49 -0400

    Now uses new API for getting the API key.

--- src/main/java/GetEmbedding.java
+++ src/main/java/GetEmbedding.java
@@ -3,11 +3,16 @@ import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
 import dev.langchain4j.model.output.Response;
 
+import static dev.langchain4j.model.openai.OpenAiEmbeddingModelName.TEXT_EMBEDDING_3_SMALL;
+
 public class GetEmbedding {
     public static void main(String[] argv) {
         String apiKey = System.getenv("OPENAI_API_KEY");
         
-        EmbeddingModel model = OpenAiEmbeddingModel.withApiKey(apiKey);
+        EmbeddingModel model = OpenAiEmbeddingModel.builder()
+                        .apiKey(apiKey)
+                        .modelName(TEXT_EMBEDDING_3_SMALL)
+                        .build();
 
         Response<Embedding> response = model.embed("One fish, two fish, red fish, blue fish");
         Embedding embedding = response.content();
